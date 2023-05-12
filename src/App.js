878026d1215ac5f09add79fd9d3bbc1903b2aea1
zenjause <jesus.nf22@gmail.com>
2023-05-11 19:24:40 -0500

    terminando proyecto react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import './assets/css/base/base.css';
 import './assets/css/componentes/card.css'
+import Home from "./pages/Home"
+import Page404 from './pages/Page404';
+import Sobre from "./pages/Sobre"
+import Header from './Components/Header';
+import Post from './pages/Posts';
+
+import Categoria from './pages/Categoria';
+import { BrowserRouter as Router , Routes,Route } from "react-router-dom"
+
 
 function App() {
-  return (
-    <>
 
-    </>
+  return (
+    <Router>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/sobre' element={<Sobre/>}/>
+        <Route path='/posts/:id' element={<Post/>}/>
+        <Route path='/categoria/:id*' element={<Categoria/>}/>
+        <Route path='*' element={<Page404/>}/>
+      </Routes>
+    </Router>
   );
 }
 
